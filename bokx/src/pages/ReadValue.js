7e0a152e2b73daf7e46016b2b595841a5980e6a7
Miruna-Miru <mirmirunalini@gmail.com>
2024-06-18 09:47:23 +0530

    Modified ReadValu

--- bokx/src/pages/ReadValue.js
+++ bokx/src/pages/ReadValue.js
@@ -1,32 +1,28 @@
 import { React, useEffect, useState } from 'react'
-import { Table } from 'react-bootstrap'
 import axios from 'axios';
 
 
 export default function ReadValue() {
-    
-    const [APIData, setAPIData] = useState([]);
+
+    const [ReadData, setReadData] = useState([]);
     useEffect(() => {
         axios.get(`https://666fc1610900b5f872481ed7.mockapi.io/Login`)
             .then((response) => {
-                setAPIData(response.data);
+                setReadData(response.data);
             })
     }, [])
     return (
         <div className='bdy'>
-            <h1>Data</h1>
-            <Table>
-                <tbody>
-  {APIData.map((data) => {
-     return (
-       <tr>
-          <td>{data.Email}</td>
-           <td>{data.Password}</td>
-           
-           </tr>
-   )})}
-   </tbody>
-</Table>
+            <center><h1>Active accounts </h1></center>
+
+            {ReadData.map((data) => {
+                return (
+                    <ul>
+                        <li>{data.Email}</li>
+                    </ul>
+                )
+            })}
+
         </div>
     )
 }
